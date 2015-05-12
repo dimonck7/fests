326317cc4b17e261519b5310e53d0a0942ee1d8c
Dima <dimonck7@gmail.com>
2015-05-13 01:19:47 +0300

    Add munu with link and some posts

--- fests/urls.py
+++ fests/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from fests.views import IndexView
+from fests.views import IndexView, ContentView, Post1View, Post2View
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$',IndexView.as_view(), name='home'),
+    url(r'^content/',ContentView.as_view(),name='content'),
+    url(r'^post1/', Post1View.as_view(),name='post1'),
+    url(r'^post2/', Post2View.as_view(),name='post2'),
 ]
--- fests/views.py
+++ fests/views.py
@@ -3,5 +3,11 @@ from django.views.generic import TemplateView
 class IndexView(TemplateView):
    template_name = "fest/index.html"
 
+class ContentView(TemplateView):
+   template_name = "fest/content.html"   
 
+class Post1View(TemplateView):
+   template_name = "fest/post1.html"
 
+class Post2View(TemplateView):
+   template_name = "fest/post2.html"
